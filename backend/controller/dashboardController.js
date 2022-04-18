413f3c8d3f7c6441070ac65598ee09b60028c992
Azmin <azmin.suhail@hexainitiative.com>
2022-04-18 16:39:20 +0800

    [] express promise handler

--- backend/controller/dashboardController.js
+++ backend/controller/dashboardController.js
@@ -1,30 +1,36 @@
+const asyncHandler = require('express-async-handler')
+
 // @ desc Get something
 // @rout GET /api/dashboard
-const getSomething = (req, res) => {
+const getSomething = asyncHandler (async (req, res) => {
     res.status(200).json({message: 'Get something'})
-
-}
+})
 
 // @ desc SET something
 // @rout POST /api/dashboard
-const setSomething = (req, res) => {
+const setSomething = asyncHandler (async (req, res) => {
+    if(!req.body.text){
+        res.status(400)
+        throw new Error('Please add a text field')
+    }
+
     res.status(200).json({message: 'Set something'})
 
-}
+})
 
 // @ desc Update something
 // @rout PUT /api/dashboard/:id
-const updateSomething = (req, res) => {
+const updateSomething = asyncHandler (async (req, res) => {
     res.status(200).json({message: `Update something ${req.params.id}`})
 
-}
+})
 
 // @ desc Delete something
 // @rout DELETE /api/dashboard
-const deleteSomething = (req, res) => {
+const deleteSomething = asyncHandler (async (req, res) => {
     res.status(200).json({message: `Delete something ${req.params.id}`})
 
-}
+})
 
 module.exports = {
     getSomething,
