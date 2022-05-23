ed83edcd6e83364606c249c10a3d07607ca4b245
Azmin <azmin.suhail@hexainitiative.com>
2022-05-24 04:17:28 +0800

    [] try

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
 const path = require('path')
 const fs = require('fs')
+const mongodb = require('mongodb')
 
 const connectDB = async() => {
 
@@ -10,25 +11,26 @@ const connectDB = async() => {
         fs.writeFileSync(caCertificatePath, process.env.CA_CERT);
 
 
-        console.log(process.env.CA_CERT)
-        console.log(caCertificatePath)
-
     }
     console.log("caCertificatePath")
 
 
     
     try {
-        const conn = await mongoose.connect(process.env.DATABASE_URL, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-            ssl: true,
-            sslValidate: true,
+        // const conn = await mongoose.connect(process.env.DATABASE_URL, {
+        //     useNewUrlParser: true,
+        //     useUnifiedTopology: true,
+        //     ssl: true,
+        //     sslValidate: true,
+        //     sslCA: caCertificatePath,
+        //    })
+
+        const conn = new mongodb.MongoClient(process.env.DATABASE_URL, {
             sslCA: caCertificatePath,
-           })
+        });
         
 
-        console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline)
+        console.log(`MongoDB Connected: ${conn}`.cyan.underline)
     } catch (error) {
         console.log(error)
         process.exit(1)
