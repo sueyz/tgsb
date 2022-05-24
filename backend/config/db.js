ebf32648af9721bda7869a1910e6899c020db657
Azmin <azmin.suhail@hexainitiative.com>
2022-05-24 08:44:22 +0800

    sqqqqqqqq

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,39 +4,11 @@ const fs = require('fs')
 const mongodb = require('mongodb')
 
 const connectDB = async() => {
-
-    let caCertificatePath = ""
-    if (process.env.CA_CERT) {
-        caCertificatePath = path.resolve("./ca-certificate.crt")
-
-        console.log(caCertificatePath)
-
-        fs.writeFileSync(caCertificatePath, process.env.CA_CERT);
-
-    }
-
-    console.log(process.env.DATABASE_URL)
-    console.log(process.env.CA_CERT)
-
-
-
-
     
     try {
-        const conn = await mongoose.connect(process.env.DATABASE_URL, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-            ssl: true,
-            sslValidate: true,
-            sslCA: caCertificatePath,
-           })
+        const conn = await mongoose.connect(process.env.DATABASE_URL)
 
 
-        // const conn =  await new mongodb.MongoClient(process.env.DATABASE_URL, {
-        //     sslCA: caCertificatePath,
-        // });
-
-        
 
         console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline)
     } catch (error) {
